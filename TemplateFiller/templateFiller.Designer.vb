897df2a75aa31e1034641d542e4edb4ac589ec2d
Markel-Kortajarena <markelkortajarena@gmail.com>
2023-04-20 01:03:38 +0200

    Agregar archivos de proyecto.

---
+++ TemplateFiller/templateFiller.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class templateFiller
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(templateFiller))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtTemplate = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.columnMarcadores = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.columnValores = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.btnCargar = New System.Windows.Forms.Button()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(88, 13)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Plantilla cargada:"
+        '
+        'txtTemplate
+        '
+        Me.txtTemplate.Location = New System.Drawing.Point(12, 25)
+        Me.txtTemplate.Name = "txtTemplate"
+        Me.txtTemplate.ReadOnly = True
+        Me.txtTemplate.Size = New System.Drawing.Size(377, 20)
+        Me.txtTemplate.TabIndex = 10
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.columnMarcadores, Me.columnValores})
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 51)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(497, 251)
+        Me.DataGridView1.TabIndex = 9
+        '
+        'columnMarcadores
+        '
+        Me.columnMarcadores.Frozen = True
+        Me.columnMarcadores.HeaderText = "MARCADORES"
+        Me.columnMarcadores.Name = "columnMarcadores"
+        Me.columnMarcadores.ReadOnly = True
+        Me.columnMarcadores.Width = 150
+        '
+        'columnValores
+        '
+        Me.columnValores.Frozen = True
+        Me.columnValores.HeaderText = "VALORES"
+        Me.columnValores.Name = "columnValores"
+        Me.columnValores.Width = 300
+        '
+        'btnCargar
+        '
+        Me.btnCargar.Image = Global.TemplateFiller.My.Resources.Resources.upload
+        Me.btnCargar.Location = New System.Drawing.Point(395, 22)
+        Me.btnCargar.Name = "btnCargar"
+        Me.btnCargar.Size = New System.Drawing.Size(114, 25)
+        Me.btnCargar.TabIndex = 13
+        Me.btnCargar.Text = "Cargar Archivo"
+        Me.btnCargar.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnCargar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.btnCargar.UseVisualStyleBackColor = True
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Image = Global.TemplateFiller.My.Resources.Resources.confirm
+        Me.btnGuardar.Location = New System.Drawing.Point(376, 323)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(133, 26)
+        Me.btnGuardar.TabIndex = 11
+        Me.btnGuardar.Text = "Guardar Documento"
+        Me.btnGuardar.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnGuardar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        Me.OpenFileDialog1.Filter = "All Files|*.docx"
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.Filter = "All Files|*.docx"
+        '
+        'templateFiller
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(521, 361)
+        Me.Controls.Add(Me.btnCargar)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.txtTemplate)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "templateFiller"
+        Me.Text = "Template Filler"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnCargar As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents txtTemplate As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents columnMarcadores As DataGridViewTextBoxColumn
+    Friend WithEvents columnValores As DataGridViewTextBoxColumn
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+End Class
---
+++ TemplateFiller/templateFiller.vb
@@ -0,0 +1,88 @@
+﻿Imports System
+Imports System.IO
+Imports System.IO.File
+Imports System.Net.WebRequestMethods
+Imports Microsoft.Office.Interop.Word
+
+Public Class templateFiller
+
+    Dim templateWord As Microsoft.Office.Interop.Word.Application
+    Dim documentoWord As Microsoft.Office.Interop.Word.Document
+
+    Private Sub templateFiller_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            Dim templateWord As Microsoft.Office.Interop.Word.Application
+            Dim documentoWord As Microsoft.Office.Interop.Word.Document
+        Catch ex As Exception
+            MsgBox("No se ha podido iniciar Microsoft Word", vbCritical)
+        End Try
+    End Sub
+
+    Private Sub btnCargar_Click(sender As Object, e As EventArgs) Handles btnCargar.Click
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+
+            templateWord = New Microsoft.Office.Interop.Word.Application()
+            documentoWord = templateWord.Documents.Add(OpenFileDialog1.FileName)
+
+            If documentoWord.Bookmarks.Count = 0 Then
+                MsgBox("La plantilla seleccionada no contiene marcadores para ser sustituidos por valores." + Environment.NewLine + "Por favor, seleccione una plantilla con marcadores.", vbExclamation)
+            Else
+                For i = 1 To documentoWord.Bookmarks.Count
+                    Me.DataGridView1.Rows.Add(documentoWord.Bookmarks(i).Name)
+                Next
+
+                txtTemplate.Text = OpenFileDialog1.FileName
+
+                documentoWord.Close(WdSaveOptions.wdDoNotSaveChanges)
+                templateWord.Quit()
+            End If
+
+        End If
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        Try
+
+            templateWord = New Microsoft.Office.Interop.Word.Application()
+            documentoWord = templateWord.Documents.Add(OpenFileDialog1.FileName)
+
+            For fila = 0 To DataGridView1.Rows.Count - 1
+
+                For marcador = 1 To documentoWord.Bookmarks.Count
+                    If documentoWord.Bookmarks(marcador).Name = DataGridView1.Item(0, fila).Value.ToString Then
+                        documentoWord.Bookmarks(marcador).Range.Text = DataGridView1.Item(1, fila).Value.ToString
+                        Exit For
+                    End If
+                Next
+
+            Next
+
+            If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
+                documentoWord.SaveAs(SaveFileDialog1.FileName, 16)
+                MsgBox("El documento se ha guardado correctamente", vbInformation)
+                For fila = 0 To DataGridView1.Rows.Count - 1
+                    DataGridView1.Item(1, fila).Value = ""
+                Next
+            Else
+                MsgBox("No se ha podido guardar el documento.", vbCritical)
+            End If
+            
+
+        Catch ex2 As Exception
+
+        Finally
+            documentoWord.Close(WdSaveOptions.wdDoNotSaveChanges)
+            templateWord.Quit()
+        End Try
+
+    End Sub
+
+    Private Sub templateFiller_Closing(sender As Object, e As EventArgs) Handles MyBase.Closing
+        Try
+            documentoWord.Close(WdSaveOptions.wdDoNotSaveChanges)
+            templateWord.Quit()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
